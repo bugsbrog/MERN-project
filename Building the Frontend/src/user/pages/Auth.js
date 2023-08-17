5aaad530dd52139fb7312b57622d33fbb7ec120f
Hannah Brog <bugsbrog@gmail.com>
2023-08-17 15:51:46 -0600

    Adding Authenticated & Unauthenticated Routes

--- Building the Frontend/src/user/pages/Auth.js
+++ Building the Frontend/src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
@@ -9,9 +9,11 @@ import {
 	VALIDATOR_MINLENGTH,
 	VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
+import { AuthContext } from "../../shared/context/auth-context";
 import "./Auth.css";
 
 const Auth = () => {
+	const auth = useContext(AuthContext);
 	const [isLoginMode, setIsLoginMode] = useState(true);
 
 	const [formState, inputHandler, setFormData] = useForm(
@@ -55,6 +57,7 @@ const Auth = () => {
 	const authSubmitHandler = (e) => {
 		e.preventDefault();
 		console.log(formState.inputs);
+		auth.login();
 	};
 
 	return (
