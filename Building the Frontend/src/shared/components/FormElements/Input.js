6811650b7ff664de083ab2a5b1889b15e5b59e2d
Hannah Brog <bugsbrog@gmail.com>
2023-08-17 11:49:49 -0600

    Sharing Input Values & Adding Multiple Inputs

--- Building the Frontend/src/shared/components/FormElements/Input.js
+++ Building the Frontend/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 
 import { validate } from "../../util/validators";
 import "./Input.css";
@@ -27,6 +27,13 @@ const Input = (props) => {
 		isTouched: false,
 		isValid: false,
 	});
+
+	const { id, onInput } = props;
+	const { value, isValid } = inputState;
+
+	useEffect(() => {
+		onInput(id, value, isValid);
+	}, [id, value, isValid, onInput]);
 
 	const changeHandler = (e) => {
 		dispatch({ type: "CHANGE", val: e.target.value, validators: props.validators });
@@ -57,7 +64,11 @@ const Input = (props) => {
 		);
 
 	return (
-		<div className={`form-control ${!inputState.isValid && inputState.isTouched && "form-control--invalid"}`}>
+		<div
+			className={`form-control ${
+				!inputState.isValid && inputState.isTouched && "form-control--invalid"
+			}`}
+		>
 			<label htmlFor={props.id}>{props.label}</label>
 			{element}
 			{!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
