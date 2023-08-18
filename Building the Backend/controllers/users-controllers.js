5e32766ceb84dacb4d654cb4fd2302724bb88fff
Hannah Brog <bugsbrog@gmail.com>
2023-08-18 14:01:55 -0600

    Using the User Model for Signup

--- Building the Backend/controllers/users-controllers.js
+++ Building the Backend/controllers/users-controllers.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
 	{
@@ -16,29 +17,44 @@ const getUsers = (req, res, next) => {
 	res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		throw new HttpError("Invalid inputs passed, check your data.", 422);
+		return next(new HttpError("Invalid inputs passed, check your data.", 422));
 	}
 
-	const { name, email, password } = req.body;
+	const { name, email, password, places } = req.body;
 
-	const hasUser = DUMMY_USERS.find((u) => u.email === email);
-	if (hasUser) {
-		throw new HttpError("Could not create user, email already exists.", 422);
+	let existingUser;
+	try {
+		existingUser = await User.findOne({ email: email });
+	} catch (err) {
+		const error = new HttpError("Sign up failed, please try again later.", 500);
+		return next(error);
 	}
 
-	const createdUser = {
-		id: uuidv4(),
-		name, // name: name
+	if (existingUser) {
+		const error = new HttpError("User already exists, please login instead.", 422);
+		return next(error);
+	}
+
+	const createdUser = new User({
+		name,
 		email,
+		image:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
 		password,
-	};
+		places,
+	});
 
-	DUMMY_USERS.push(createdUser);
+	try {
+		await createdUser.save();
+	} catch (err) {
+		const error = new HttpError("Signing up failed, please try again.", 500);
+		return next(error);
+	}
 
-	res.json(201).json({ user: createdUser });
+	res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
