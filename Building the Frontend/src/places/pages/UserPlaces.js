ef490ae04b59ca7cf5c985948c012d6782145d3b
Hannah Brog <bugsbrog@gmail.com>
2023-08-20 21:31:32 -0600

    Updating Places

--- Building the Frontend/src/places/pages/UserPlaces.js
+++ Building the Frontend/src/places/pages/UserPlaces.js
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import PlaceList from "../components/PlaceList";
-import ErrorModal from "../../shared/components/UIElements/ErrorModal";
-import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
-import { useHttpClient } from "../../shared/hooks/http-hook";
+import PlaceList from '../components/PlaceList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const UserPlaces = () => {
-	const [loadedPlaces, setLoadedPlaces] = useState();
-	const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-	const userId = useParams().userId;
+  const userId = useParams().userId;
 
-	useEffect(() => {
-		const fetchPlaces = async () => {
-			try {
-				const responseData = await sendRequest(
-					`http://localhost:5001/api/places/user/${userId}`,
-				);
-				setLoadedPlaces(responseData.places);
-			} catch (err) {}
-		};
-		fetchPlaces();
-	}, [sendRequest, userId]);
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5001/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
 
-	return (
-		<React.Fragment>
-			<ErrorModal error={error} onClear={clearError} />
-			{isLoading && (
-				<div className="center">
-					<LoadingSpinner />
-				</div>
-			)}
-			{!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;
