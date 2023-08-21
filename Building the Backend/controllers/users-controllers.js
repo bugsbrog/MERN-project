d2adc69e99a7198525b0af72aa4bec73d63f0595
Hannah Brog <bugsbrog@gmail.com>
2023-08-21 15:25:59 -0600

    Authorization on "Delete Places"

--- Building the Backend/controllers/users-controllers.js
+++ Building the Backend/controllers/users-controllers.js
@@ -93,7 +93,7 @@ const login = async (req, res, next) => {
 	}
 
 	if (!existingUser) {
-		const error = new HttpError("Invalid credentials, could not log you in.", 401);
+		const error = new HttpError("Invalid credentials, could not log you in.", 403);
 		return next(error);
 	}
 
@@ -110,7 +110,7 @@ const login = async (req, res, next) => {
 	}
 
 	if (!isValidPassword) {
-		const error = new HttpError("Invalid credentials, could not log you in.", 401);
+		const error = new HttpError("Invalid credentials, could not log you in.", 403);
 		return next(error);
 	}
 
