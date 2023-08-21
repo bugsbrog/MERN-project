7b45c41a5d60745a5c37c7eae3b96c34deedbc7b
Hannah Brog <bugsbrog@gmail.com>
2023-08-21 15:36:23 -0600

    Adding Auto-Login (Basic Version)

--- Building the Frontend/src/App.js
+++ Building the Frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
 import Auth from "./user/pages/Auth";
@@ -22,7 +22,15 @@ const App = () => {
 	const logout = useCallback(() => {
 		setToken(null);
 		setUserId(null);
+		localStorage.removeItem("userData");
 	}, []);
+
+	useEffect(() => {
+		const storedData = JSON.parse(localStorage.getItem("userData"));
+		if (storedData && storedData.token) {
+			login(storedData.userId, storedData.token);
+		}
+	}, [login]);
 
 	let routes;
 
