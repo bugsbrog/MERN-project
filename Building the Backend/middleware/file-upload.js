b3fddd43a60b63c79538ea92392db78fa97aeb98
Hannah Brog <bugsbrog@gmail.com>
2023-08-21 13:37:13 -0600

    Filtering Files on the Backend(Images Only!)

--- Building the Backend/middleware/file-upload.js
+++ Building the Backend/middleware/file-upload.js
@@ -15,9 +15,15 @@ const fileUpload = multer({
 		},
 		filename: (req, file, cb) => {
 			const ext = MIME_TYPE_MAP[file.mimetype];
-			cb(null, uuid() + "." + ext);
+			cb(null, uuidv1() + "." + ext);
 		},
 	}),
+	fileFilter: (req, file, cb) => {
+		// "!!" converts undefined to false
+		const isValid = !!MIME_TYPE_MAP[file.mimetype];
+		let error = isValid ? null : new Error("Invalid mime type!");
+		cb(error, isValid);
+	},
 });
 
 module.exports = fileUpload;
